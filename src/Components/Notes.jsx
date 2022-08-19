f109f115d83145c6cb58a26b14766276d79a98f1
chamudij <chamudij99@gmail.com>
2022-08-19 17:41:02 +0530

    Source files

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is a note</h1>
+      <p>You can make many notes like this using this Note Taking App</p>
+    </div>
+  );
+}
+
+export default Notes;
